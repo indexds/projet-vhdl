dfce18fed2091ef9d767692efb274d05488fbd3c
_index <indexd@pm.me>
2024-03-21 09:50:42 +0100

    xdc add

--- led.vhd
+++ led.vhd
@@ -7,17 +7,19 @@ entity led is
 port (CLK,RST : in std_logic;
         L: out std_logic);
 end led;
+
 architecture rtl of led is
-signal cnt : std_logic_vector(23 downto 0);
-begin
-process (CLK,RST)
-begin
-    if (RST = '1') then
-        cnt <= (others => '0');
-    elsif (CLK'event and CLK = '1') then
-        cnt <= cnt +1;
-    end if;
-end process;
+
+    signal cnt : std_logic_vector(23 downto 0);
+    begin
+        process (CLK,RST)
+        begin
+            if (RST = '1') then
+                cnt <= (others => '0');
+            elsif (CLK'event and CLK = '1') then
+                cnt <= cnt +1;
+            end if;
+        end process;
 
 L <= cnt(23);
 
